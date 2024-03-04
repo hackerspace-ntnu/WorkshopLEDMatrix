170fa419c54db1d7368aab39c4b7a68b253f55db
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2024-03-04 13:19:55 +0100

    found another example of TCP connection, this actually works

--- esp32_tcpClient/main/tcp_server.c
+++ esp32_tcpClient/main/tcp_server.c
@@ -1,27 +1,66 @@
-#include <stdio.h>
 #include <string.h>
+#include <sys/param.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-#include "freertos/semphr.h"
-#include "freertos/queue.h"
-#include "freertos/timers.h"
+#include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
-#include "lwip/err.h"
-#include "lwip/sys.h"
-#include "lwip/netdb.h"
-#include "lwip/sockets.h"
 #include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
 #include "tcp_server.h"
 
 
+#define PORT                        CONFIG_WIFI_IPV4_PORT
+#define KEEPALIVE_IDLE              CONFIG_TCP_KEEPALIVE_IDLE
+#define KEEPALIVE_INTERVAL          CONFIG_TCP_KEEPALIVE_INTERVAL
+#define KEEPALIVE_COUNT             CONFIG_TCP_KEEPALIVE_COUNT
+#define WIFI_SSID                   CONFIG_ESP_WIFI_SSID
+#define WIFI_PASSWORD               CONFIG_ESP_WIFI_PASSWORD
+#define ESP_MAXIMUM_RETRY           CONFIG_ESP_MAXIMUM_RETRY
+#define WIFI_CONNECTED_BIT          BIT0
+#define WIFI_FAIL_BIT               BIT1
+
+static const char *TAG = "TCP_SERVER";
+
+static void do_retransmit(const int sock) {
+    int len;
+    char rx_buffer[128];
+
+    do {
+        len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+        if (len < 0) {
+            ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+        } else if (len == 0) {
+            ESP_LOGW(TAG, "Connection closed");
+        } else {
+            rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
+            ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
+
+            // send() can return less bytes than supplied length.
+            // Walk-around for robust implementation.
+            int to_write = len;
+            while (to_write > 0) {
+                int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
+                if (written < 0) {
+                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                    // Failed to retransmit, giving up
+                    return;
+                }
+                to_write -= written;
+            }
+        }
+    } while (len > 0);
+}
 
 static EventGroupHandle_t s_wifi_event_group;
 static int s_retry_num = 0;
-static const char *TAG = "esp32_server";
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -29,7 +68,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+        if (s_retry_num < ESP_MAXIMUM_RETRY) {
             esp_wifi_connect();
             s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
@@ -45,24 +84,9 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
-void client_handler(int client_sock) {
-    char buffer[256];
-    int len = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
-    if (len > 0) {
-        buffer[len] = '\0';
-        ESP_LOGI(TAG, "Received data from client: %s", buffer);
-
-        const char *response = "Hello there!";
-        send(client_sock, response, strlen(response), 0);
-        ESP_LOGI(TAG, "Sent response to client: %s", response);
-    }
-
-    close(client_sock);
-}
-
 void wifi_init_sta() {
 
-     s_wifi_event_group = xEventGroupCreate();
+    s_wifi_event_group = xEventGroupCreate();
 
     ESP_ERROR_CHECK(esp_netif_init());
 
@@ -88,7 +112,7 @@ void wifi_init_sta() {
     wifi_config_t sta_config = {
         .sta = {
             .ssid = WIFI_SSID,
-            .password = WIFI_PASS,
+            .password = WIFI_PASSWORD,
             /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (pasword len => 8).
              * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
              * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
@@ -96,7 +120,7 @@ void wifi_init_sta() {
              */
             // .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
             // .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
-            .sae_pwe_h2e = WPA3_SAE_PWE_UNSPECIFIED,
+            // .sae_pwe_h2e = WPA3_SAE_PWE_UNSPECIFIED,
             // .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
         },
     };
@@ -116,48 +140,98 @@ void wifi_init_sta() {
      * happened. */
     if (bits & WIFI_CONNECTED_BIT) {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                WIFI_SSID, WIFI_PASS);
+                WIFI_SSID, WIFI_PASSWORD);
     } else if (bits & WIFI_FAIL_BIT) {
         ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                WIFI_SSID, WIFI_PASS);
+                WIFI_SSID, WIFI_PASSWORD);
     } else {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 }
 
-void start_server() {
-    struct sockaddr_in server_addr;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(12345);
+static void tcp_server_task(void *pvParameters) {
+    char addr_str[128];
+    int addr_family = (int)pvParameters;
+    int ip_protocol = 0;
+    int keepAlive = 1;
+    int keepIdle = KEEPALIVE_IDLE;
+    int keepInterval = KEEPALIVE_INTERVAL;
+    int keepCount = KEEPALIVE_COUNT;
+    struct sockaddr_storage dest_addr;
 
-    int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-    bind(listen_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    listen(listen_sock, 5);
 
-    TaskHandle_t xHandle = NULL;
-    while (1) {
-        struct sockaddr_in client_addr;
-        socklen_t client_addr_len = sizeof(client_addr);
-        int client_sock = accept(listen_sock, (struct sockaddr*)&client_addr, &client_addr_len);
-
-        if (client_sock != -1) {
-            // Handle the client in a separate task
-            xTaskCreate((void*)client_handler, "client_handler", 4096, (void*)&client_sock, 5, &xHandle);
-
-            if (xHandle != NULL) {
-                vTaskDelete(xHandle);
-            }
-        }
+    if (addr_family == AF_INET) {
+        struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
+        dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+        dest_addr_ip4->sin_family = AF_INET;
+        dest_addr_ip4->sin_port = htons(PORT);
+        ip_protocol = IPPROTO_IP;
     }
+
+    int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+    if (listen_sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        vTaskDelete(NULL);
+        return;
+    }
+    int opt = 1;
+    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    ESP_LOGI(TAG, "Socket created");
+
+    int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (err != 0) {
+        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        ESP_LOGE(TAG, "IPPROTO: %d", addr_family);
+        goto CLEAN_UP;
+    }
+    ESP_LOGI(TAG, "Socket bound, port %d", PORT);
+
+    err = listen(listen_sock, 1);
+    if (err != 0) {
+        ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
+        goto CLEAN_UP;
+    }
+
+    while (1) {
+
+        ESP_LOGI(TAG, "Socket listening");
+
+        struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+        socklen_t addr_len = sizeof(source_addr);
+        int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+            break;
+        }
+
+        // Set tcp keepalive option
+        setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &keepAlive, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &keepIdle, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &keepInterval, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keepCount, sizeof(int));
+        // Convert ip address to string
+        if (source_addr.ss_family == PF_INET) {
+            inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+        }
+
+        ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
+
+        do_retransmit(sock);
+
+        shutdown(sock, 0);
+        close(sock);
+    }
+
+CLEAN_UP:
+    close(listen_sock);
+    vTaskDelete(NULL);
 }
 
-void app_main() {
+void start_server() {
     ESP_ERROR_CHECK(nvs_flash_init());
     
     wifi_init_sta();
-    start_server();
+    xTaskCreate(tcp_server_task, "tcp_server", 4096, (void*)AF_INET, 5, NULL);
 }
-
-
 
