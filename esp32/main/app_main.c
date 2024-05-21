9d4403a9fd74c4f221cbea6a3a9d13278ec47b29
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2024-05-21 12:50:46 +0200

    removed previous pattern and changed the setup-funtion

--- esp32/main/app_main.c
+++ esp32/main/app_main.c
@@ -24,15 +24,13 @@ led_strip_handle_t setup_led_strip(bool use_spi, uint32_t number_of_leds, int gp
 
     if (use_spi) {
         led_strip_spi_config_t spi_config = {
-            .clk_src = SOC_MOD_CLK_APB, // different clock source can lead to different power consumption, 
-                                        // from clk_tree_defs SPI_CLK_SRC_DEFAULT is set to SOC_MOD_CLK_APB 
+            .clk_src = SPI_CLK_APB,         // different clock source can lead to different power consumption
             .flags.with_dma = true,         // Using DMA can improve performance and help drive more LEDs
             .spi_bus = SPI2_HOST,           // SPI bus ID
         };
-
         
         ESP_ERROR_CHECK(led_strip_new_spi_device(&led_strip_config, &spi_config, &led_strip));
-        ESP_LOGI(TAG, "Created LED strip object with SPI backend");
+        ESP_LOGI(TAG, "Created LED strip object using SPI");
         
     } else {
        
@@ -41,14 +39,13 @@ led_strip_handle_t setup_led_strip(bool use_spi, uint32_t number_of_leds, int gp
                     .rmt_channel = 0,
             #else
                 .clk_src = RMT_CLK_SRC_DEFAULT,        // different clock source can lead to different power consumption
-                .resolution_hz = LED_STRIP_RMT_RES_HZ, // RMT counter clock frequency
+                .resolution_hz = 10000000,             // RMT counter clock frequency:  10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
                 .flags.with_dma = false,               // DMA feature is available on ESP target like ESP32-S3
             #endif
         };
 
-        led_strip_handle_t led_strip;
         ESP_ERROR_CHECK(led_strip_new_rmt_device(&led_strip_config, &rmt_config, &led_strip));
-        ESP_LOGI(TAG, "Created LED strip object with RMT backend");
+        ESP_LOGI(TAG, "Created LED strip object using RMT");
     }
     
     return led_strip;
@@ -65,15 +62,10 @@ void app_main(void)  {
     led_strip_handle_t led_strip = setup_led_strip(use_spi, number_of_leds, gpio_pin);
     bool is_led_on = false;
 
-    // ESP_LOGI(TAG, "Start blinking LED strip");
     while (1) {
         if (is_led_on) {
-            // uint8_t pattern[CONFIG_ESP_LED_STRIP_LED_NUMBER] = {
-            //     0,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,
-            // };
-            // Map the pattern to the LED strip
             for (int i = 0; i < number_of_leds; i++) {
-                ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, ));
+                ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 255, 255, 255));
             }
 
             /* Refresh the strip to send data */
@@ -88,7 +80,4 @@ void app_main(void)  {
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
-
-
-
 
